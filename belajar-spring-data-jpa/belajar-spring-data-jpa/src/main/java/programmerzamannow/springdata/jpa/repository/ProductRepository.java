aff74607a4e3197b56977a8fffafe4a7b413787a
JovHariono <128022304+JovHariono@users.noreply.github.com>
2025-08-02 22:42:44 +0700

    180

--- belajar-spring-data-jpa/belajar-spring-data-jpa/src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
+++ belajar-spring-data-jpa/belajar-spring-data-jpa/src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import programmerzamannow.springdata.jpa.entity.Product;
 
 import java.util.List;
@@ -22,5 +26,22 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     boolean existsByName(String name);
 
+    @Transactional
     int deleteByName(String name);
+
+    List<Product> searchProductUsingName(@Param("name")  String name, Pageable pageable);
+
+    @Query(
+            value = "SELECT p FROM Product p WHERE p.name LIKE :name or p.category.name LIKE :name",
+            countQuery = "SELECT COUNT(p) FROM Product p WHERE p.name LIKE :name or p.category.name LIKE :name"
+    )
+    Page<Product> searchProduct(@Param("name") String name, Pageable pageable);
+
+    @Modifying
+    @Query(value = "DELETE FROM Product p WHERE p.name = :name")
+    int deleteProductUsingName(@Param("name") String name);
+
+    @Modifying
+    @Query(value = "UPDATE Product p SET p.price = 0 WHERE p.id = :id")
+    int updateProductUsingId(@Param("id") Long id);
 }
--- belajar-spring-data-jpa/belajar-spring-data-jpa/src/test/java/programmerzamannow/springdata/jpa/ProductRepositoryTest.java
+++ belajar-spring-data-jpa/belajar-spring-data-jpa/src/test/java/programmerzamannow/springdata/jpa/ProductRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionOperations;
 import programmerzamannow.springdata.jpa.entity.Category;
 import programmerzamannow.springdata.jpa.entity.Product;
@@ -106,7 +108,7 @@ public class ProductRepositoryTest {
     }
 
     @Test
-    void testDelete() {
+    void testDeleteOld() {
         transactionOperations.executeWithoutResult(transactionStatus -> {
             Category category = categoryRepository.findById(1L).orElse(null);
             Assertions.assertNotNull(category);
@@ -122,6 +124,71 @@ public class ProductRepositoryTest {
 
             delete  = productRepository.deleteByName("Samsung Galaxy S9");
             Assertions.assertEquals(0, delete);
+        });
+    }
+
+    @Test
+    void testDelete() {
+//        transactionOperations.executeWithoutResult(transactionStatus -> {
+            Category category = categoryRepository.findById(1L).orElse(null);
+            Assertions.assertNotNull(category);
+
+            Product product = new Product();
+            product.setName("Samsung Galaxy S9");
+            product.setPrice(10_000_000L);
+            product.setCategory(category);
+            productRepository.save(product);
+
+            int delete = productRepository.deleteByName("Samsung Galaxy S9");
+            Assertions.assertEquals(1, delete);
+
+            delete  = productRepository.deleteByName("Samsung Galaxy S9");
+            Assertions.assertEquals(0, delete);
+//        });
+    }
+
+    @Test
+    void searchProductUsingName() {
+        Pageable pageable = PageRequest.of(0, 1, Sort.by(Sort.Order.desc("id")));
+
+        List<Product> products = productRepository.searchProductUsingName("Apple iPhone 14 Pro Max", pageable);
+        Assertions.assertEquals(1, products.size());
+        Assertions.assertEquals("Apple iPhone 14 Pro Max", products.get(0).getName());
+
+    }
+
+    @Test
+    void searchProduct() {
+        Pageable pageable= PageRequest.of(0, 1, Sort.by(Sort.Order.desc("id")));
+
+        Page<Product> products = productRepository.searchProduct("Apple%", pageable);
+        Assertions.assertEquals(1, products.getContent().size());
+
+
+        Assertions.assertEquals(0, products.getNumber());
+        Assertions.assertEquals(2, products.getTotalPages());
+        Assertions.assertEquals(2, products.getTotalElements());
+
+        products = productRepository.searchProduct("GADGET MURAH", pageable);
+        Assertions.assertEquals(1, products.getContent().size());
+
+        Assertions.assertEquals(0, products.getNumber());
+        Assertions.assertEquals(2, products.getTotalPages());
+        Assertions.assertEquals(2, products.getTotalElements());
+    }
+
+    @Test
+    void modifying() {
+        transactionOperations.executeWithoutResult(TransactionStatus -> {
+            int total = productRepository.deleteProductUsingName("Wrong");
+            Assertions.assertEquals(0, total);
+
+            total = productRepository.updateProductUsingId(1L);
+            Assertions.assertEquals(1, total);
+
+            Product product = productRepository.findById(1L).orElse(null);
+            Assertions.assertNotNull(product);
+            Assertions.assertEquals(0L, product.getPrice());
         });
     }
 }
